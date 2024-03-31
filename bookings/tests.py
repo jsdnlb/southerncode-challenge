c04a695fdd26c097fdd28b1bd88daee2ff2a3326
Daniel Buitrago <danielb.alzate1@gmail.com>
2024-03-30 21:29:52 -0500

    Add new cases test bookings using DocStrings and improve calculate final price func

--- bookings/tests.py
+++ bookings/tests.py
@@ -85,6 +85,20 @@ class BookingCreateTestCase(APITestCase):
         )
 
     def test_create_booking_case_1(self):
+        """
+        Test case for creating a booking with a discount applied.
+
+        This test verifies that a booking is created successfully without applying any discounts.
+        The booking is for a property starting on January 1, 2022, and ending on January 10, 2022.
+
+        Steps:
+        1. Create booking data with the property ID and dates for the booking.
+        2. Send a POST request to create the booking.
+        3. Verify that the response status code is 201 (Created).
+        4. Verify that the final price calculated for the booking is 90.
+        5. Verify that the stay length of the booking is calculated correctly as 10 days.
+
+        """
         booking_data = {"property": self.property_one.pk}
         booking_data.update(self.START_01_01_2022_END_01_10_2022)
         response = self.client.post(self.list_url, booking_data, format="json")
@@ -93,7 +107,24 @@ class BookingCreateTestCase(APITestCase):
         self.assertEqual(response.data["final_price"], 90)
         self.assertEqual(response.data["stay_length"], 10)
 
+    def test_create_booking_case_1_without_discount(self):
+        """
+        Test case for creating a booking without any discount applied.
+        This case there are 1 rules of the type min_stay_length and price_modifier
+        """
+        booking_data = {"property": self.property_one.pk}
+        booking_data.update(self.START_01_01_2022_END_01_03_2022)
+        response = self.client.post(self.list_url, booking_data, format="json")
+
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(response.data["final_price"], 30)
+        self.assertEqual(response.data["stay_length"], 3)
+
     def test_create_booking_case_2(self):
+        """
+        Test case for creating a booking with a discount applied.
+        This case there are 2 rules of the type min_stay_length and price_modifier
+        """
         booking_data = {"property": self.property_two.pk}
         booking_data.update(self.START_01_01_2022_END_01_10_2022)
         response = self.client.post(self.list_url, booking_data, format="json")
@@ -103,7 +134,15 @@ class BookingCreateTestCase(APITestCase):
         self.assertEqual(response.data["stay_length"], 10)
 
     def test_create_booking_case_2_greater_than_30_days(self):
-        """Validates that you take the largest discount since it complies with the 30-day rule, ignoring the 7-day rule"""
+        """
+        Test case for creating a booking with a stay length greater than 30 days.
+
+        This test verifies that a booking is created successfully when the stay length exceeds 30 days.
+        In this scenario, the largest discount is applied since it complies with the 30-day rule,
+        ignoring the 7-day rule. The booking is for a property starting on January 1, 2022,
+        and ending on February 14, 2022.
+        """
+
         booking_data = {"property": self.property_two.pk}
         booking_data.update(self.START_01_01_2022_END_02_14_2022)
 
@@ -114,7 +153,15 @@ class BookingCreateTestCase(APITestCase):
         self.assertEqual(response.data["stay_length"], 45)
 
     def test_create_booking_case_2_equal_to_30_days(self):
-        """Validates that you take the largest discount since it complies with the 30-day rule, ignoring the 7-day rule"""
+        """
+        Test case for creating a booking with a stay length equal to 30 days.
+
+        This test verifies that a booking is created successfully when the stay length is exactly 30 days.
+        In this scenario, the largest discount is applied since it complies with the 30-day rule,
+        ignoring the 7-day rule. The booking is for a property starting on January 1, 2022,
+        and ending on January 30, 2022.
+
+        """
         booking_data = {
             "property": self.property_two.pk,
             "start_date": "01-01-2022",
@@ -127,6 +174,10 @@ class BookingCreateTestCase(APITestCase):
         self.assertEqual(response.data["stay_length"], 30)
 
     def test_create_booking_case_3(self):
+        """
+        Test case for creating a booking with a discount applied.
+        This case there are 1 rules of the type min_stay_length and price_modifier and  1 rules of the type specific_day and fixed_price
+        """
         booking_data = {"property": self.property_three.pk}
         booking_data.update(self.START_01_01_2022_END_01_10_2022)
 
@@ -137,6 +188,10 @@ class BookingCreateTestCase(APITestCase):
         self.assertEqual(response.data["stay_length"], 10)
 
     def test_create_booking_case_4(self):
+        """
+        Test case for creating a booking with a discount applied.
+        This case there are 1 rules of the type min_stay_length and price_modifier and 2 rules of the type specific_day and fixed_price
+        """
         booking_data = {"property": self.property_four.pk}
         booking_data.update(self.START_01_01_2022_END_01_10_2022)
 
@@ -146,7 +201,27 @@ class BookingCreateTestCase(APITestCase):
         self.assertEqual(response.data["final_price"], 154.2)
         self.assertEqual(response.data["stay_length"], 10)
 
+    def test_create_booking_case_4_without_discount(self):
+        """
+        Test case for creating a booking without any discount applied.
+
+        This test verifies that a booking is created successfully without applying any discounts.
+        The booking is for a property starting on January 1, 2022, and ending on January 3, 2022.
+        """
+        booking_data = {"property": self.property_four.pk}
+        booking_data.update(self.START_01_01_2022_END_01_03_2022)
+
+        response = self.client.post(self.list_url, booking_data, format="json")
+
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(response.data["final_price"], 45)
+        self.assertEqual(response.data["stay_length"], 3)
+
     def test_create_booking_case_5(self):
+        """
+        Test case for creating a booking with a discount applied using decimals in the base price
+        This case there are 3 rules of the type min_stay_length and price_modifier
+        """
         booking_data = {
             "property": self.property_five.pk,
             "start_date": "01-01-2022",
@@ -159,8 +234,31 @@ class BookingCreateTestCase(APITestCase):
         self.assertEqual(response.data["final_price"], 355.52)
         self.assertEqual(response.data["stay_length"], 44)
 
+    def test_create_booking_case_5_without_discount(self):
+        """
+        Test case for creating a booking without any discount applied.
+
+        This test verifies that a booking is created successfully without applying any discounts.
+        The booking is for a property starting on January 1, 2022, and ending on January 3, 2022.
+
+        """
+        booking_data = {"property": self.property_five.pk}
+        booking_data.update(self.START_01_01_2022_END_01_03_2022)
+
+        response = self.client.post(self.list_url, booking_data, format="json")
+
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(response.data["final_price"], 30.3)
+        self.assertEqual(response.data["stay_length"], 3)
+
     def test_create_booking_case_5_equal_to_45_days(self):
-        """Forcing third rule"""
+        """
+        Test the creation of a booking for property_five for a 45-day period.
+        This test case validates that a booking is successfully created for the property_five
+        property from January 1, 2022, to February 14, 2022. This test specifically checks
+        if the third pricing rule is applied.
+        """
+
         booking_data = {"property": self.property_five.pk}
         booking_data.update(self.START_01_01_2022_END_02_14_2022)
 
@@ -171,6 +269,11 @@ class BookingCreateTestCase(APITestCase):
         self.assertEqual(response.data["stay_length"], 45)
 
     def test_create_booking_case_6(self):
+        """
+        Test case for creating a booking with a increase applied
+        This case there are 2 rules of the type min_stay_length and price_modifier
+        The exception with this one is that instead of having a discount it increases the value
+        """
         booking_data = {"property": self.property_six.pk}
         booking_data.update(self.START_01_01_2022_END_01_03_2022)
 
@@ -181,6 +284,14 @@ class BookingCreateTestCase(APITestCase):
         self.assertEqual(response.data["stay_length"], 3)
 
     def test_create_booking_case_6_greater_than_7_days(self):
+        """
+        Test the creation of a booking for a stay longer than 7 days.
+
+        This test case validates that a booking is successfully created for the property_six
+        property for a stay from January 1, 2022, to January 10, 2022, which is longer than
+        7 days. This test specifically checks if the pricing rule for stays longer than 7 days
+        is correctly applied.
+        """
         booking_data = {"property": self.property_six.pk}
         booking_data.update(self.START_01_01_2022_END_01_10_2022)
 
--- bookings/utils.py
+++ bookings/utils.py
@@ -4,16 +4,33 @@ from pricing_rules.models import PricingRule
 from properties.models import Property
 
 
-# We add +1 one to count the start date
-# By default it only returns the value between the dates but without counting the start date.
-# Calculate stay_length dynamically
 def calculate_stay_length(start_date: date, end_date: date) -> int:
+    """
+    Calculate the length of stay between two dates, including both the start and end dates.
+    Add +1 one to count the start date, by default it only returns the value between the dates but without counting the start date.
+
+    Args:
+        start_date (date): The start date of the stay.
+        end_date (date): The end date of the stay.
+
+    Returns:
+        int: The length of stay in days
+    """
     return (end_date - start_date).days + 1
 
 
-# The rules are ordered by min_stay_length from smallest to largest, thus ensuring that regardless of the order in which the price rules are registered, they are applied correctly.
-# It also ensures that elements with None are placed at the beginning of the sort.
 def sort_pricing_rules(pricing_rules: List[Dict]) -> List[Dict]:
+    """
+    Sort pricing rules based on the minimum stay length in ascending order.
+    It also ensures that elements with None are placed at the beginning of the sort.
+
+    Args:
+        pricing_rules (List[Dict]): A list of pricing rules, where each rule is represented as a dictionary.
+
+    Returns:
+        List[Dict]: A list of pricing rules sorted in ascending order based on the minimum stay length.
+        Rules with no minimum stay length are placed at the end.
+    """
     return sorted(
         pricing_rules,
         key=lambda x: (
@@ -25,6 +42,16 @@ def sort_pricing_rules(pricing_rules: List[Dict]) -> List[Dict]:
 
 
 def get_pricing_rules(property_id: Property) -> List[Dict]:
+    """
+    Get pricing rules associated with a property and sort them based on the minimum stay length.
+
+    Args:
+        property_id (Property): The ID of the property for which pricing rules are retrieved.
+
+    Returns:
+        List[Dict]: A list of pricing rules associated with the property, sorted based on the minimum stay length.
+        Each rule is represented as a dictionary containing the fields 'min_stay_length', 'price_modifier', 'specific_day', and 'fixed_price'.
+    """
     pricing_rules = PricingRule.objects.filter(property_id=property_id).values(
         "min_stay_length", "price_modifier", "specific_day", "fixed_price"
     )
@@ -38,32 +65,66 @@ def calculate_final_price(
     stay_length: int,
     base_price: float,
 ) -> float:
+    """
+    Calculates the final price of a booking applying pricing rules.
+
+    Args:
+        pricing_rules (List[Dict]): A list of dictionaries containing applicable pricing rules.
+        start_date (date): The start date of the booking.
+        end_date (date): The end date of the booking.
+        stay_length (int): The length of stay in days.
+        base_price (float): The base price per day of the property.
+
+    Returns:
+        float: The final price of the booking.
+
+    Notes:
+        The count_specific_day is used to use the formula to add the final price
+        It has a valid condition when there is no discount applied, it is not used within the loop,
+        since all the rules are validated there with the values "min_stay_length", "price_modifier", "specific_day", "fixed_price"
+    """
     final_price = 0
-    new_stay_length = stay_length
     count_specific_day = False
 
-    # The count_specific_day is used to use the formula to add the final price
     for rule in pricing_rules:
-        if rule["specific_day"] and rule["fixed_price"]:
-            if start_date <= rule["specific_day"] <= end_date:
-                final_price += rule["fixed_price"]
-                new_stay_length = new_stay_length - 1
+        specific_day = rule.get("specific_day")
+        fixed_price = rule.get("fixed_price")
+        min_stay_length = rule.get("min_stay_length")
+        price_modifier = rule.get("price_modifier")
+
+        if specific_day is not None and fixed_price is not None:
+            if start_date <= specific_day <= end_date:
+                final_price += fixed_price
+                stay_length -= 1
                 count_specific_day = True
 
-        if rule["min_stay_length"] and rule["price_modifier"]:
-            if stay_length >= rule["min_stay_length"]:
-                new_base_price = base_price * new_stay_length
-                percent_rule = rule["price_modifier"] / 100
-                price = new_base_price + (new_base_price * percent_rule)
+        if min_stay_length is not None and price_modifier is not None:
+            if stay_length >= min_stay_length:
+                new_base_price = base_price * stay_length
+                price = new_base_price + (new_base_price * price_modifier / 100)
                 if count_specific_day:
                     final_price += price
                 else:
                     final_price = price
 
-    return final_price
+    if final_price == 0 and stay_length > 1 and base_price > 0.1:
+        final_price = base_price * stay_length
+
+    return round(final_price, 2)
 
 
-def create_property_with_rules(property_data, rules_data):
+def create_property_with_rules(property_data: dict, rules_data: List[dict]) -> Property:
+    """
+    Create a property along with pricing rules.
+
+    Args:
+        property_data (dict): A dictionary containing the data for creating the property.
+        rules_data (List[dict]): A list of dictionaries, where each dictionary contains the data for creating
+        a pricing rule associated with the property.
+
+    Returns:
+        Property: The created property object.
+    """
     property = Property.objects.create(**property_data)
     for rule_data in rules_data:
         PricingRule.objects.create(property=property, **rule_data)
